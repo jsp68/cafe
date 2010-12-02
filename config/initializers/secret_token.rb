363a81fb7fae5e0c675eb86b1ee0a08b9e81d3d0
Jeff Posey <jeff.posey@protective.com>
2010-12-02 09:18:59 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cafe::Application.config.secret_token = 'f5d432bcf05dfb7491413d8dc5da3b747dd03906311c679644b12326958080d7d002a2021af49d5f14937bae183a8ec2e692b1fecca666c63890b1b76f22d67e'
